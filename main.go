d74b14ee57fbf90f057aa8b5fabcbf7e1debd729
Andreas Koch <andyk7@gmail.com>
2016-04-15 21:58:50 +0200

    Refactoring

--- main.go
+++ main.go
@@ -14,20 +14,34 @@ import (
 // const GoogleAnalyticsHostname = "www.googleapis.com"
 const GoogleAnalyticsHostname = "www-googleapis-com-yb0hxtzk6st4.runscope.net"
 
-// GoogleAnalyticsClientID contains the client ID of the Google API credentials (see: https://console.developers.google.com/apis/credentials)
-const GoogleAnalyticsClientID = "821429244906-8aki1tiaov6g2o7lr7elp41435adk9ge.apps.googleusercontent.com"
+// getAnalyticsClientConfig returns the oAuth client configuration for the Google Analytics API.
+func getAnalyticsClientConfig(clientId, clientSecret, redirectURL string) *oauth2.Config {
+	return &oauth2.Config{
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectURL,
+		Scopes: []string{
+			"https://www.googleapis.com/auth/analytics",
+			"https://www.googleapis.com/auth/analytics.edit",
+			"https://www.googleapis.com/auth/analytics.readonly",
+		},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://accounts.google.com/o/oauth2/auth",
+			TokenURL: "https://accounts.google.com/o/oauth2/token",
+		},
+	}
+}
 
-// GoogleAnalyticsClientSecret contains the client secret of the Google API credentials (see: https://console.developers.google.com/apis/credentials)
-const GoogleAnalyticsClientSecret = "_WxLj0SpQ8HxqmOEyYDUTFzW"
-
-// receiveAuthorizationCode return a redirect URL and a channel which
-// receives the Google oAuth authorization code once the user has
-// authorized the operation.
-func receiveAuthorizationCode() (string, chan string) {
+// receiveAuthorizationCode returns the authorization code.
+func receiveAuthorizationCode(conf *oauth2.Config, listenAddress, route string) (string, error) {
 
 	authorizationCode := make(chan string, 1)
-	listenAddress := "localhost:8080"
-	route := "/authorizationCodeReceiver"
+
+	// Redirect user to consent page to ask for permission
+	// for the scopes specified above.
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+	fmt.Printf("Visit the URL for the auth dialog: %v", url)
+	fmt.Println()
 	go func() {
 
 		listener, err := net.Listen("tcp", listenAddress)
@@ -53,57 +67,48 @@ func receiveAuthorizationCode() (string, chan string) {
 		http.Serve(handler, nil)
 	}()
 
-	return fmt.Sprintf("http://%s%s", listenAddress, route), authorizationCode
-}
-
-// getAnalyticsClient returns a Google Analytics client instance.
-func getAnalyticsClient() *http.Client {
-	redirectURL, codeReceiver := receiveAuthorizationCode()
-
-	conf := &oauth2.Config{
-		ClientID:     GoogleAnalyticsClientID,
-		ClientSecret: GoogleAnalyticsClientSecret,
-		RedirectURL:  redirectURL,
-		Scopes: []string{
-			"https://www.googleapis.com/auth/analytics",
-			"https://www.googleapis.com/auth/analytics.edit",
-			"https://www.googleapis.com/auth/analytics.readonly",
-		},
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://accounts.google.com/o/oauth2/auth",
-			TokenURL: "https://accounts.google.com/o/oauth2/token",
-		},
-	}
-
-	// Redirect user to consent page to ask for permission
-	// for the scopes specified above.
-	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
-	fmt.Println()
-
 	// Use the authorization code that is pushed to the redirect URL.
 	// NewTransportWithCode will do the handshake to retrieve
 	// an access token and initiate a Transport that is
 	// authorized and authenticated by the retrieved token.
-	code := <-codeReceiver
+	code := <-authorizationCode
 	if len(code) == 0 {
-		log.Fatal("No authorization code received.")
+		return "", fmt.Errorf("No authorization code received.")
 	}
 
-	log.Printf("Authorization code received: %s\n", code)
+	return code, nil
+}
 
-	tok, err := conf.Exchange(oauth2.NoContext, code)
+// getAnalyticsClient returns a Google Analytics client instance.
+func getAnalyticsClient(oAuthClientConfig *oauth2.Config, listenAddress, route string) *http.Client {
+	code, err := receiveAuthorizationCode(oAuthClientConfig, listenAddress, route)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	client := conf.Client(oauth2.NoContext, tok)
+	tok, err := oAuthClientConfig.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := oAuthClientConfig.Client(oauth2.NoContext, tok)
 	return client
 }
 
 func main() {
 
-	client := getAnalyticsClient()
+	// credentials
+	googleAnalyticsClientID := "821429244906-8aki1tiaov6g2o7lr7elp41435adk9ge.apps.googleusercontent.com"
+	googleAnalyticsClientSecret := "_WxLj0SpQ8HxqmOEyYDUTFzW"
+
+	// oAuth code receiver
+	listenAddress := "localhost:8080"
+	route := "/authorizationCodeReceiver"
+	redirectURL := fmt.Sprintf("http://%s%s", listenAddress, route)
+
+	// oAuth client config
+	oAuthClientConfig := getAnalyticsClientConfig(googleAnalyticsClientID, googleAnalyticsClientSecret, redirectURL)
+	client := getAnalyticsClient(oAuthClientConfig, listenAddress, route)
 	getAccounts(client)
 
 	accountId := "578578"
