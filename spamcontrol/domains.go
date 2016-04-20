8351f4ee072ae73e433d0699af36851f3b9184cf
Andreas Koch <andyk7@gmail.com>
2016-04-20 07:49:19 +0200

    Fetch spam domains from remote source

--- spamcontrol/domains.go
+++ spamcontrol/domains.go
@@ -1,5 +1,12 @@
 package spamcontrol
 
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
 // A spamDomainProvider provides domain names of
 // referer spam providers.
 type spamDomainProvider interface {
@@ -15,5 +22,26 @@ type remoteSpamDomainProvider struct {
 
 // GetSpamDomains returns a list of referer spam domain names.
 func (spamDomainProvider *remoteSpamDomainProvider) GetSpamDomains() ([]string, error) {
-	return []string{"0n-line.tv", "100dollars-seo.com"}, nil
+
+	// request the domain names from the remote source
+	response, requestError := http.Get(spamDomainProvider.domainListUrl)
+	if requestError != nil {
+		return nil, fmt.Errorf("Failed to get URL %q: %s", spamDomainProvider.domainListUrl, requestError.Error())
+	}
+
+	// read the domain names line-by-line
+	var domains []string
+	scanner := bufio.NewScanner(response.Body)
+	for scanner.Scan() {
+		domain := strings.TrimSpace(scanner.Text())
+
+		// ignore empty lines
+		if domain == "" {
+			continue
+		}
+
+		domains = append(domains, domain)
+	}
+
+	return domains, nil
 }
