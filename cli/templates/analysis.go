05e3ddb8d88e24c61a687438c2b941d993e3c834
Andreas Koch <andyk7@gmail.com>
2016-05-05 17:24:44 +0200

    Print message if no spam domains were detected

--- cli/templates/analysis.go
+++ cli/templates/analysis.go
@@ -2,13 +2,19 @@ package templates
 
 // PrettyAnalysis contains the detailed template for displaying
 // the spam analysis results for a given analytics account.
-var PrettyAnalysis = `{{printf "%45s" "Domainname"}} Probability
+var PrettyAnalysis = `{{if .SpamDomains -}}
+{{printf "%45s" "Domainname"}} Probability
 {{range .SpamDomains -}}
 {{printf "%45s" .DomainName}} {{printf "%.2f" .SpamProbability}}
-{{end}}`
+{{end -}}
+{{else -}}
+No referrer spam domains detected
+{{- end}}`
 
 // QuietAnalysis contains the minimal  template for displaying
 // the spam analysis results for a given analytics account.
-var QuietAnalysis = `{{range .SpamDomains -}}
+var QuietAnalysis = `{{if .SpamDomains -}}
+{{range .SpamDomains -}}
 {{.DomainName}}
-{{end}}`
+{{end}}
+{{- end}}`
